f72425d1359f77facd010c5d9cac9bd8594d7e6b
ajaykumar-bg <ajaykumarbg.tvm@gmail.com>
2025-02-22 12:16:23 +0530

    Moved language Translation to a separate feature folder

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import LanguageSelector from './LanguageSelector';
-import { useTranslation, Trans } from 'react-i18next';
+import LanguageTranslator from './features/lang-translation/LanguageTranslation';
 
 function App() {
-	const { t, i18n, ready } = useTranslation();
-
-	const changeLanguage = (lan) => {
-		i18n.changeLanguage(lan);
-	};
-	if (ready) {
-		return (
-			<div className='App'>
-				<header className='App-header'>
-					<LanguageSelector
-						pCurrentLang={i18n.language}
-						pOnChange={changeLanguage}
-					/>
-					<img src={logo} className='App-logo' alt='logo' />
-					<p>{t('welcomeMessage')}</p>
-					<a
-						className='App-link'
-						href='https://reactjs.org'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						{t('subtitle')}
-					</a>
-					<p>
-						<Trans i18nKey='settings'></Trans>
-					</p>
-					<p>
-						<Trans i18nKey='profile'></Trans>
-					</p>
-				</header>
-			</div>
-		);
-	}
-	return <span>Loading...</span>;
+	return (
+		<div className='App'>
+			<LanguageTranslator />
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+			</header>
+		</div>
+	);
 }
 
 export default App;
